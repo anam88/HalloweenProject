5b9a5bd6aeea994f257d176ade6cb69d68146135
anam88 <amasroor88@gmail.com>
2020-10-31 14:38:57 -0500

    Halloween Project

--- HalloweenProject/src/test/java/HalloweenTest.java
+++ HalloweenProject/src/test/java/HalloweenTest.java
@@ -2,5 +2,6 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("Today is Halloween");
         System.out.println("Today is October31");
+        System.out.println("Today is Saturday");
     }
 }
