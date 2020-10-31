a0274bd2dd6acd7984243dbee5eaa96e6c0db425
anam88 <amasroor88@gmail.com>
2020-10-31 13:59:04 -0500

    another commit

--- HalloweenProject/src/test/java/HalloweenTest.java
+++ HalloweenProject/src/test/java/HalloweenTest.java
@@ -1,5 +1,6 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("Today is Halloween");
+        System.out.println("Today is October31");
     }
 }
